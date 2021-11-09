0d29d2bfe060dfc53830b14d4956868233de1b37
sofiene <sofiene.chaari@gmail.com>
2021-11-09 10:36:45 +0100

    correction copie du cle ssh

--- 7.4/core/docker-entrypoint.sh
+++ 7.4/core/docker-entrypoint.sh
@@ -32,7 +32,7 @@ if [ -n "$FILES_PATH" ]; then
     chmod 777 $FILES_PATH
   fi
 fi
-cp /var/www/ssh/* /var/www/.ssh/
+cp -R /var/www/ssh/* /var/www/.ssh/
 /etc/init.d/cron start
 # Apache gets grumpy about PID files pre-existing
 rm -f /var/run/apache2/apache2.pid
